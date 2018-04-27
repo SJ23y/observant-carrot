0423accf9dd73fc310e513fbe6f14fb0b99138ec
Glitch (observant-carrot) <none>
2018-04-27 16:22:36 +0000

    🌱🏛 Checkpoint ./foo.js:997454/114

--- foo.js
+++ foo.js
@@ -1,5 +1,5 @@
 var mongo = require('mongodb').MongoClient
-var mongo_url = ''
+var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
 mongo.connect(mongo_url, function(err,db) {
     if (err) {console.log('Error occured')}
     
