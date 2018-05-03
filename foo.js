bca5a64e227504aa40db1205eaeb73bee5c6f6b6
Glitch (observant-carrot) <none>
2018-05-03 17:34:25 +0000

    🐽🍓 Checkpoint ./foo.js:997454/81

--- foo.js
+++ foo.js
@@ -1,8 +1,7 @@
 var mongo = require('mongodb').MongoClient
-var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT
+var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB
 mongo.connect(mongo_url, function(err,db) {
     if (err) {console.log('Error occured')}
-    var chopper = db.db('chopper')
-    chopper.collection('urls')
+    db.db(').collection('urls')
     db.close()
 })
