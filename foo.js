9d350c3dde6eef6a8318c8ab9a3df18f76cc96c5
Glitch (observant-carrot) <none>
2018-05-03 17:44:50 +0000

    🏤🐿 Checkpoint ./foo.js:997454/91

--- foo.js
+++ foo.js
@@ -2,6 +2,7 @@ var mongo = require('mongodb').MongoClient
 var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB
 mongo.connect(mongo_url, function(err,db) {
     if (err) {console.log('Error occured')}
-    db.db(').collection('urls')
+    var urls = db.db('chopper').collection('urls')
+    urls.insert([{"_id": 1, "url": 'https://yandex.ru'}])
     db.close()
 })
