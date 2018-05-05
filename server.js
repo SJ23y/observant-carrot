8564766f646365c0b9bc191b9a97de0d9c4b5dfd
Glitch (observant-carrot) <none>
2018-05-05 12:56:07 +0000

    🦃👲 Checkpoint ./server.js:997454/4

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.route('/new/*').get(function(req, res) {
     var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB
     mongo.connect(mongo_url, function(err,db) {      
     if (err) {console.log('Error occured')}
-      if (req.url.slice(5).match('^[http|https]\/\/:[.+]+?[\.\w+]{2,3}') == null) 
+      if (req.url.slice(5).match('^[http|https]\/\/:[.+?\.]+[\.\w+]{2,3}') == null) 
       {
         res.send('Invalid Url')
         db.close()                                                                                
