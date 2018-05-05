d9c2653ad992afff6dcde7b669805eab6794b2d9
Glitch (observant-carrot) <none>
2018-05-05 13:16:58 +0000

    🎳🏭 Checkpoint ./server.js:997454/537

--- server.js
+++ server.js
@@ -22,22 +22,21 @@ app.route('/new/*').get(function(req, res) {
     var mongo_url = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB
     mongo.connect(mongo_url, function(err,db) {      
     if (err) {console.log('Error occured')}
-      var patt = new RegExp('^(http|https):\/\/.+\.\w{2,3}')
-      if (req.url.slice(5).match(patt) == null) 
+      var patt = new RegExp('^https?:\/\/((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}')
+      if (!patt.test(req.url.slice(5))) 
       {
         res.send('Invalid Url')
-        db.close()                                                                                
-                                                                               }
-      else {
-    var urls = db.db('chopper').collection('urls')
-    urls.aggregate( [ { $group : {'_id': null, count: { $sum: 1 } } } ] ).toArray(function(err, doc) {
-        var ID = doc[0].count
-        var short_url = 'https://observant-carrot.glitch.me/' + ID
-        urls.insert( [ { "_id": parseInt(ID),"url": req.url.slice(5) } ] )    
-        res.send( { "original_url": req.url.slice(5), "short_url": short_url } ) 
-        db.close()    
-  })  
-      }
+        db.close()
+      } else {
+        var urls = db.db('chopper').collection('urls')
+            urls.aggregate( [ { $group : {'_id': null, count: { $sum: 1 } } } ] ).toArray(function(err, doc) {
+            var ID = doc[0].count
+            var short_url = 'https://observant-carrot.glitch.me/' + ID
+            urls.insert( [ { "_id": parseInt(ID),"url": req.url.slice(5) } ] )    
+            res.send( { "original_url": req.url.slice(5), "short_url": short_url } ) 
+            db.close()    
+                                                                                                            })  
+              }
     
     })
   
